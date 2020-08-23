b925e9a07cdb3e1b26c46f3d8030a4aff48b8f8a
Ahmed5510-Mac <54468849+Ahmed5510-Mac@users.noreply.github.com>
2020-08-23 16:05:22 +0200

    test Dom

---
+++ js/js.js
@@ -0,0 +1,33 @@
+// document.getElementById("")
+// document.getElementsByClassName("")
+// document.getElementsByName
+// document.getElementsByTagName
+// document.querySelector
+// // document.querySelectorAllfunction\
+// function h1n()
+// {
+//    // let ele=document.getElementById("hell");
+//    // ele.style.backgroundColor="red";
+//    // ele.style.display="inline-block";
+//    // ele.style.position="relative";
+//    // ele.style.left="30%";
+//    // ele.innerHTML="hahhahhhhhhh welcom user"
+
+// //    let ele= document.querySelector(".one")
+// //    //ele.classList.add("rad")
+// //    ele .classList.remove("one")
+//    
+//    console.log(Image);
+// }
+
+   let amge=document.getElementsByClassName("img-fluid");
+
+   for(i=0;i<amge.length;i++)
+   {
+      amge[i].addEventListener("mousmove" , syhello );
+   }
+   
+   function syhello()
+   {
+      alert("hello mr ahmed");
+   }
---
+++ js/nwe.js
@@ -0,0 +1,9 @@
+let amg=document.getElemlentById("ahmed55")
+
+    document.addEventListener("mousmove",moveImage)
+    function moveImage(e)
+    {
+        amg.style.left = e .clinetx;
+        amg.style.left= e.clinety;
+    }
+    console.lofg(clinetx)
